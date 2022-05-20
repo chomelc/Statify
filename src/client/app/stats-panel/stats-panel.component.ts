4721dc9dad4a8ecb0809e88bb954ca5fb3bcc2c9
Clémence CHOMEL <clemence.chomel@gmail.com>
2022-05-20 22:40:19 +0200

    Fixing some CSS for the tabs

--- src/client/app/stats-panel/stats-panel.component.ts
+++ src/client/app/stats-panel/stats-panel.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTabGroup } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-stats-panel',
@@ -6,7 +7,20 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./stats-panel.component.css'],
 })
 export class StatsPanelComponent implements OnInit {
+  @ViewChild('matTabGroup', { static: true })
+  matTabGroup!: MatTabGroup;
+
+  onMatTabChanged() {
+    this.setMatTabGroup();
+  }
+
+  setMatTabGroup() {
+    let ntvEl = this.matTabGroup._elementRef.nativeElement;
+    ntvEl.style.minHeight = ntvEl.clientHeight + 'px';
+  }
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.setMatTabGroup();
+  }
 }
